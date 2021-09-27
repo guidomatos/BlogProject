79a651e9a2a5153e0df95e12486f02193dc8abfb
guido.matos <guido.matos.88@gmail.com>
2021-09-26 21:45:11 -0500

    Creacion de componentes Menu y Card

---
+++ src/components/Menu/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './style.css';
+import Card from '../UI/Card';
+
+const Menu = (props) => {
+  return(
+    <div>
+        <Card>
+            
+        </Card>
+    </div>
+   )
+
+ }
+
+export default Menu
