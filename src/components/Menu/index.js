b19d4392b04182d05b0d18e312495db2fa7d621f
guido.matos <guido.matos.88@gmail.com>
2021-09-26 21:48:34 -0500

    Creacion de componente Logo

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './style.css';
 import Card from '../UI/Card';
+import Logo from '../Logo';
 
 const Menu = (props) => {
   return(
     <div>
         <Card>
-            
+            <div style={{ padding: '50px 0' }}>
+                <Logo />
+            </div>
         </Card>
     </div>
    )
