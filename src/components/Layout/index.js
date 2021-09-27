a97f00921113e5d5c5467499641e5419f4460a50
guido.matos <guido.matos.88@gmail.com>
2021-09-26 22:44:35 -0500

    Creacion de componente Layout

---
+++ src/components/Layout/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './style.css';
+
+const Layout = (props) => {
+    return(
+        <>
+            <div className="container">
+                {props.children}
+            </div>
+        </>
+    )
+}
+
+export default Layout
